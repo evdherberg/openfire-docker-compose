390ceffcff6996e8150a0eeede9adc57ab061f66
Emiel van der Herberg <emiel.van.der.herberg@marviq.com>
2021-07-28 13:31:23 +0200

    - Added third Openfire server to the clustered setup. - Added alternative network to move nodes to, in order to remove them from the main cluster while still being able to reach them. - Modified start script for my own convenience, which assumes the openfire repository is cloned in the same place as this docker compose repository.

---
+++ get-logs.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+rm *.log
+docker exec -t openfire-docker-compose_xmpp1_1 cat /usr/local/openfire/logs/all.log > 1.log
+docker exec -t openfire-docker-compose_xmpp2_1 cat /usr/local/openfire/logs/all.log > 2.log
+docker exec -t openfire-docker-compose_xmpp3_1 cat /usr/local/openfire/logs/all.log > 3.log
