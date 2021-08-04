8be1bc1396471994b47135762ebf08e54b97909e
Emiel van der Herberg <emiel.van.der.herberg@marviq.com>
2021-08-04 22:05:09 +0200

    Apply Pumba for enforcing cluster disruptions. And, since there is no need for it anymore, remove the "alternative" network again.

--- to-alt.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-docker network disconnect openfire-docker-compose_openfire-clustered-net openfire-docker-compose_xmpp$1_1
-docker network connect --ip 172.70.0.$10 openfire-docker-compose_openfire-alternative-net openfire-docker-compose_xmpp$1_1
